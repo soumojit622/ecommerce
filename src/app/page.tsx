5964d3fa3d775a4372f1a0d2e523b9c55e4fabae
Soumojit Banerjee <soumojitbanerjee22@gmail.com>
2025-08-12 01:20:04 +0530

    some components customized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,27 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div>
-      <Button>Hello there</Button>
+    <div className="p-5">
+      <div>
+        <Button variant="elevated">Hello there</Button>
+      </div>
+      <div className="mt-5">
+        <Input placeholder="hello there" />
+      </div>
+      <div className="mt-5">
+        <Progress value={50} />
+      </div>
+      <div className="mt-5">
+        <Textarea value="hello there" />
+      </div>
+      <div className="mt-5">
+        <Checkbox />
+      </div>
     </div>
   );
 }
